486a675889e4fa09c07c301dadb94f8190baa0d6
KatarinaSt <stanojlovic.katarina.852@gmail.com>
2024-12-16 12:14:43 +0100

    first commit

---
+++ src/test/java/rs/heliant/moneo/MoneoApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.heliant.moneo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
